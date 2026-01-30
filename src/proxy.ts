22e91f68f2e82ad372d3c5fd3392828372d3f669
Anirudh030703 <anirudh.s@adventsys.co>
2026-01-30 16:51:32 +0530

    SNYK vulnerability fixes

--- src/proxy.ts
+++ src/proxy.ts
@@ -7,7 +7,7 @@ interface DecodedToken {
   [key: string]: unknown;
 }
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   const token = request.cookies.get("token")?.value;
   const { pathname } = request.nextUrl;
 
